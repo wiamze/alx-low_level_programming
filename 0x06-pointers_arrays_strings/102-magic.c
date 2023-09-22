d41a34b90f53b337ac2d27e4ae1d36fccfc627b2
Wiam Zerhouni <09.heaps-pharaoh@icloud.com>
2023-09-22 06:42:58 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,22 +2,19 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
- 	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */		
-	 *p = 98;
-	 /* ...so that this prints 98\n */
-	 printf("a[2] = %d\n", a[2]);
-	 return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * La ligne de code suivante modifie n à travers p en utilisant l'arithmétique des pointeurs.
+   * Elle affecte la valeur 98 à l'emplacement mémoire situé à 5 entiers de distance de l'adresse de n.
+   */
+  *(p + 5) = 98;
+  /* ...de sorte que cela affiche 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
+
